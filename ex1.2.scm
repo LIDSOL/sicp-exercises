5197bcc3e81a12c62e83d87aa485426c201b6b6e
Diego Alberto Barriga Martínez <diegobarriga@protonmail.com>
2020-05-01 13:51:51 -0500

    add(ejercicio): 1.2

---
+++ ex1.2.scm
@@ -0,0 +1,4 @@
+; Ejercicio 1.2: Traduce la siguiente expresión en forma prefija:
+; (5 + 4 + (2 - (3 - (6 + 4/5)))) / (3 * (6 - 2) * (2 - 7))
+
+(/ (+ 5 4 (- 2 (- 3 (+ 6 (/ 4 5))))) (* 3 (- 6 2) (- 2 7)))
